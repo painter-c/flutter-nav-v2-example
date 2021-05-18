af146d467266bf67834e41414937ed91442f366b
Cody <cody.pntr@gmail.com>
2021-05-18 10:31:51 -0700

    Created AppInfoPage

---
+++ lib/pages/app_info_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppInfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("app_info_page"),
+      ),
+    );
+  }
+}
